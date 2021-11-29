647746f3caa5f7099870e9312dfb215cc6229288
Bennett <bjupfield@gmail.com>
2021-11-29 14:59:11 -0600

    nice and easy

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import { useEffect } from "react";
+import { useState } from "react";
+import react from "react";
+function App(){
+    const [summonedYet, setSummonedYet] = useState(false);
+    const [pic, setPic] = useState("string");
+
+    function hanSummon(){
+        setSummonedYet(true);
+    }
+    function hanPic(z){
+        setPic(z)
+    }
+
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r=>r.json())
+        .then(r=>{
+            hanSummon()
+            hanPic(r.message)
+        })
+    }, [])
+    return <div>
+        {!summonedYet ? <p>Loading...</p> : <img src = {pic} alt = "A Random Dog"></img>}
+    </div>
+}
+export default App;
